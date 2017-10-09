94b9b60ab7a52bb0b4c18f6f8e7b780d378ba984
illestknock <skyisnotlimit@hotmail.com>
2017-10-09 17:54:40 -0400

    Add files via upload

---
+++ Week2/Bangerz.py
@@ -0,0 +1,14 @@
+#foxdot , rpg background sound
+
+bd >> play("o oo ( o)  x-- xx")
+bd.shuffle()
+
+hh >> play(" -oo ooxxxx-")
+hh.shuffle()
+
+sn >> play("-o-xx -x--oo--x ")
+sn.shuffle()
+
+d1 >> play("---- [xo] (oo)")
+b1 >> bass([(5,9),(3,17)], dur= 2, pan=(-1,1))
+b2 >> bass([(0,9),(1,7)], dur= 4, pan=(-1,1))
